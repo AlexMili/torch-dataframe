c721cfa534ffc9d507b7389fdabf4fb627eee58d
Max Gordon <max@gforge.se>
2016-04-09 12:03:19 +0200

    Added test-files

---
+++ tests/visual_output.lua
@@ -0,0 +1,24 @@
+require "../Dataframe"
+
+-- A quick way to get a feeling for how the __tostring method works
+local a = Dataframe()
+a:load_csv{path = "simple_short.csv",
+           verbose = false}
+print("-- Simple table --")
+print(a)
+
+print("-- Advanced table --")
+a:load_csv{path = "advanced_short.csv",
+           verbose = false}
+print(a)
+
+print("-- Long table --")
+a:load_csv{path = "realistic_29_row_data.csv",
+           verbose = false}
+print(a)
+
+a.print.no_rows = 5
+print(a)
+
+a.print.no_rows = 20
+print(a)
