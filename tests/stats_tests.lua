51731c0d07f885c7675100adeefaf4cc984b6f0f
Max Gordon <max@gforge.se>
2016-04-21 20:03:11 +0200

    +  value counts for multiple columns + get_mode + tests and moved value_counts into stats_test

--- tests/stats_tests.lua
+++ tests/stats_tests.lua
@@ -11,6 +11,68 @@ lfs.chdir("tests")
 local stat_tests = torch.TestSuite()
 local tester = torch.Tester()
 
+function stat_tests.value_counts()
+  local a = Dataframe()
+  a:load_csv{path = "advanced_short.csv",
+             verbose = false}
+  tester:assertTableEq(a:value_counts('Col A'), {[1] = 1, [2] = 1, [3] = 1}, "Failed to count Col A")
+  tester:assertTableEq(a:value_counts('Col B'), {A=1, B=2}, "Failed to count Col B")
+  tester:assertTableEq(a:value_counts('Col C'), {[8]=1, [9]=1}, "Failed to count Col C")
+  tester:assertTableEq(a:value_counts{column_name = 'Col C',
+                                      dropna=false},
+                       {[8]=1, [9]=1, ["_missing_"] = 1}, "Failed to count Col C with missing")
+end
+
+function stat_tests.value_counts_proportions()
+  local a = Dataframe()
+  a:load_csv{path = "advanced_short.csv",
+             verbose = false}
+  tester:assertTableEq(a:value_counts{column_name ='Col A',
+                                      normalize = true},
+                       {[1] = 1/3, [2] = 1/3, [3] = 1/3}, "Failed to count Col A")
+  tester:assertTableEq(a:value_counts{column_name ='Col B',
+                                      normalize = true},
+                       {A = 1/3, B = 2/3}, "Failed to count Col B")
+  tester:assertTableEq(a:value_counts('Col C'), {[8]=1, [9]=1}, "Failed to count Col C")
+  tester:assertTableEq(a:value_counts{column_name = 'Col C',
+                                      dropna=false},
+                       {[8]=1, [9]=1, ["_missing_"] = 1}, "Failed to count Col C with missing")
+  tester:assertTableEq(a:value_counts{column_name = 'Col C',
+                                      normalize = true,
+                                     dropna=false},
+                      {[8]=1/3, [9]=1/3, ["_missing_"] = 1/3},
+                      "Failed to count Col C with missing and normalization")
+
+  tester:assertTableEq(a:value_counts(),
+                     {['Col C'] = {[8]=1, [9]=1},
+                     ['Col A'] = {[1] = 1, [2] = 1, [3] = 1}},
+                     "Failed to count all columns with missing")
+  tester:assertTableEq(a:value_counts{dropna=false},
+                     {['Col C'] = {[8]=1, [9]=1, ["_missing_"] = 1},
+                     ['Col A'] = {[1] = 1, [2] = 1, [3] = 1, ["_missing_"] = 0}},
+                     "Failed to count all columns with missing")
+
+end
+
+function stat_tests.get_mode()
+  local a = Dataframe()
+  a:load_csv{path = "advanced_short.csv",
+             verbose = false}
+  tester:assertTableEq(a:get_mode{column_name ='Col A', normalize = false},
+                       {[1] = 1, [2] = 1, [3] = 1}, "Failed to get mode for Col A")
+  tester:assertTableEq(a:get_mode{column_name ='Col A', normalize = true},
+                       {[1] = 1/3, [2] = 1/3, [3] = 1/3}, "Failed to get mode for Col A with normalize")
+  tester:assertTableEq(a:get_mode{column_name ='Col B',
+                                  normalize = true},
+                       {B = 2/3}, "Failed to get mode for Col B")
+
+  a:load_table{data={['A']={3,3,2},['B']={10,11,12}}}
+  tester:assertTableEq(a:get_mode{normalize = true},
+                      {A ={[3] = 2/3},
+                       B ={[10] = 1/3, [11] = 1/3, [12] = 1/3}},
+                      "Failed to get mode for multiple columns")
+end
+
 function stat_tests.get_max_value()
   local a = Dataframe()
   a:load_csv{path = "advanced_short.csv",
