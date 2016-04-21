bab681e799fed7321e475219a7bfc0abdb0f5964
Max Gordon <max@gforge.se>
2016-04-21 20:03:11 +0200

    +  value counts for multiple columns + get_mode + tests and moved value_counts into stats_test

--- Extensions/statistics.lua
+++ Extensions/statistics.lua
@@ -2,48 +2,159 @@ require 'dok'
 local params = {...}
 local Dataframe = params[1]
 
---
--- value_counts() : count number of occurences for each unique element (frequency/histogram)
---
 -- ARGS: - see dok.unpack
---
--- RETURNS : table with the unique value as key and count as value
---
 function Dataframe:value_counts(...)
 	local args = dok.unpack(
 		{...},
 		'Dataframe.value_counts',
+		[[
+		Counts number of occurences for each unique element (frequency/histogram) in
+		a single column or set of columns. If a single column is requested then it returns
+		a simple table with element names as keys and counts/proportions as values.
+		If multiple keys have been requested it returns a table wrapping the single
+		column counts with column name as key.
+		]],
+		{arg='column_name', type='string', help='column to inspect'},
+		{arg='normalize', type='boolean', default=false,
+		 help=[[
+		 	If True then the object returned will contain the relative frequencies of
+			the unique values.]]},
+		{arg='dropna', type='boolean', default=true,
+		 help="Don’t include counts of NaN (missing values)."}
+	)
+	local single = false
+	if (not args.column_name) then
+		args.column_name = self:get_numerical_colnames()
+		if (#args.column_name == 1) then
+			single = true
+		end
+	elseif(type(args.column_name) == 'string') then
+		args.column_name = {args.column_name}
+		single = true
+	end
+	value_counts = {}
+	for _,cn in pairs(args.column_name) do
+		value_counts[cn] = self:_single_col_value_counts{
+			column_name = cn,
+			normalize = args.normalize,
+			dropna = args.dropna
+		}
+	end
+	if (single) then
+		return value_counts[args.column_name[1]]
+	else
+		return value_counts
+	end
+end
+
+function Dataframe:_single_col_value_counts(...)
+	local args = dok.unpack(
+		{...},
+		'Dataframe._single_col_value_counts',
 		'get value counts of elements given a column name',
-		{arg='column_name', type='string', help='column to inspect', req=true}
+		{arg='column_name', type='string', help='column to inspect', req=true},
+		{arg='normalize', type='boolean', default=false,
+		 help=[[
+			If True then the object returned will contain the relative frequencies of
+			the unique values.]]},
+		{arg='dropna', type='boolean', default=true,
+		 help="Don’t include counts of NaN (missing values)."}
 	)
 	assert(self:has_column(args.column_name),
-	       "Invalid column name: " .. tostring(args.column_name))
+				 "Invalid column name: " .. tostring(args.column_name))
 	count = {}
 
-  -- Experiencing odd behavior with large data > 254459 rows
-  --  better to use raw data and then convert the names at the end
+	-- Experiencing odd behavior with large data > 254459 rows
+	--  better to use raw data and then convert the names at the end
 	column_data = self:get_column{column_name = args.column_name,
-                                as_raw = true}
+																as_raw = true}
+	no_missing = 0
 	for i = 1,self.n_rows do
 		current_key_value = column_data[i]
-    if (not isnan(current_key_value)) then
-      assert(current_key_value ~= nil, "invalid data for row " .. i)
+		if (not isnan(current_key_value)) then
+			assert(current_key_value ~= nil, "invalid data for row " .. i)
 			if (count[current_key_value] == nil) then
 				count[current_key_value] = 1
 			else
 				count[current_key_value] = count[current_key_value] + 1
 			end
+		else
+			no_missing = no_missing + 1
 		end
 	end
-  if (self:is_categorical(args.column_name)) then
-    tmp = {}
-    for key,no_found in pairs(count) do
-      name = self:to_categorical(key, args.column_name)
-      tmp[name] = no_found
-    end
-    count = tmp
-  end
-  return count
+	if (self:is_categorical(args.column_name)) then
+		tmp = {}
+		for key,no_found in pairs(count) do
+			name = self:to_categorical(key, args.column_name)
+			tmp[name] = no_found
+		end
+		count = tmp
+	end
+	if (not args.dropna) then
+		count["_missing_"] = no_missing
+	end
+	if (args.normalize) then
+		total = 0
+		for _,n in pairs(count) do
+			total = total + n
+		end
+		for i,n in pairs(count) do
+			count[i] = n/total
+		end
+	end
+	return count
+end
+--
+-- see dok.unpack
+--
+function Dataframe:get_mode(...)
+	local args = dok.unpack(
+		{...},
+		'Dataframe.get_mode',
+		[[gets the most occurring value in a certain column, i.e. the mode.
+		If no column is provided retrieves all numerical columns in a named table.]],
+		{arg='column_name', type='string', help='column to inspect'},
+		{arg='normalize', type='boolean', default=false,
+		 help=[[
+		 	If True then the object returned will contain the relative frequencies of
+			the mode.]]},
+		{arg='dropna', type='boolean', default=true,
+		 help="Don’t include counts of NaN (missing values)."}
+	)
+	local single = false
+	if (not args.column_name) then
+		args.column_name = self:get_numerical_colnames()
+		if (#args.column_name == 1) then
+			single = true
+		end
+	elseif(type(args.column_name) == 'string') then
+		args.column_name = {args.column_name}
+		single = true
+	end
+	modes = {}
+	for i = 1,#args.column_name do
+		local cn = args.column_name[i]
+		local counts = self:_single_col_value_counts{column_name = cn,
+								                                 normalize = args.normalize,
+																								 dropna = args.dropna}
+		local max_val = 0/0
+		for _,v in pairs(counts) do
+			if (isnan(max_val) or max_val < v) then
+				max_val  = v
+			end
+		end
+		modes[cn] = {}
+		for key,v in pairs(counts) do
+			if (max_val == v) then
+				modes[cn][key] = v
+			end
+		end
+	end
+	if single then
+		return modes[args.column_name[1]]
+	else
+		return modes
+	end
 end
 
 --
