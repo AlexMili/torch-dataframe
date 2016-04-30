3d3b787d6ea743964b61d1b8e725e6cd5a44ca0f
AlexMili <AlexMili@users.noreply.github.com>
2016-04-30 22:14:23 +0200

    trim table and table equals utils

--- utils.lua
+++ utils.lua
@@ -5,6 +5,50 @@ function trim(s)
 	return s:match"^%s*()" > #s and "" or s:match(".*%S", s:match"^%s*()")
 end
 
+function trim_table_strings(t)
+	assert(type(t) == 'table', "You must provide a table")
+
+	for index,value in pairs(t) do
+		if(type(value) == 'string') then
+			t[index] = trim(value)
+		end
+	end
+
+	return t
+end
+
+-- See https://stackoverflow.com/questions/20325332/how-to-check-if-two-tablesobjects-have-the-same-value-in-lua
+function tables_equals(o1, o2, ignore_mt)
+    if o1 == o2 then return true end
+    local o1Type = type(o1)
+    local o2Type = type(o2)
+    if o1Type ~= o2Type then return false end
+    if o1Type ~= 'table' then return false end
+
+    if not ignore_mt then
+        local mt1 = getmetatable(o1)
+        if mt1 and mt1.__eq then
+            --compare using built in method
+            return o1 == o2
+        end
+    end
+
+    local keySet = {}
+
+    for key1, value1 in pairs(o1) do
+        local value2 = o2[key1]
+        if value2 == nil or tables_equals(value1, value2, ignore_mt) == false then
+            return false
+        end
+        keySet[key1] = true
+    end
+
+    for key2, _ in pairs(o2) do
+        if not keySet[key2] then return false end
+    end
+    return true
+end
+
 function clone(t) -- shallow-copy a table
 	if type(t) ~= "table" then return t end
 	local meta = getmetatable(t)
