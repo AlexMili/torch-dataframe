06b9f269d70e1892778d6e85114dbaf9f5379efc
AlexMili <mili.alexandre@gmail.com>
2016-05-08 18:22:50 +0200

    For loop perf optimization

--- Extensions/missing_data.lua
+++ Extensions/missing_data.lua
@@ -16,7 +16,9 @@ function Dataframe:count_na(...)
 		'Count mising values in dataset',
 		{arg='columns', type='string|table', help='the columns to count to labels'}
 	)
-	local single
+
+	local single = false
+	
 	if (args.columns == nil) then
 		args.columns = self.columns
 	elseif(type(args.columns) == 'string') then
@@ -25,16 +27,19 @@ function Dataframe:count_na(...)
 	else
 		error("Invalid columns argument: " .. tostring(args.columns))
 	end
-	count = {}
 
+	count = {}
 	for _,column_name in pairs(args.columns) do
 		counter = 0
+
 		for i = 1, self.n_rows do
 			local val = self.dataset[column_name][i]
+
 			if val == nil or val == '' or isnan(val) then
 				counter = counter + 1
 			end
 		end
+
 		count[column_name] = counter
 	end
 
@@ -56,6 +61,7 @@ end
 -- Enhancement : detect nil/na value at first reading or _infer_schema
 function Dataframe:fill_na(column_name, default_value)
 	assert(self:has_column(column_name), "Could not find column: " .. tostring(column_name))
+
 	if (self:count_na(column_name) == 0) then
 		return
 	end
@@ -83,7 +89,7 @@ end
 --
 -- Enhancement : detect nil/na value at first reading or _infer_schema
 function Dataframe:fill_all_na(default_value)
-	for _,key in pairs(self.columns) do
-		self:fill_na(key, default_value)
+	for i=1,#self.columns do
+		self:fill_na(self.columns[i], default_value)
 	end
 end
