cddf15a2086b18f9e55bdd54f5fc5da6b75b032e
Max Gordon <max@gforge.se>
2016-05-17 22:14:36 +0200

    Improved helper classes

--- helper_classes/dict.lua
+++ helper_classes/dict.lua
@@ -6,6 +6,13 @@ local dict = torch.class('Df_Dict')
 function dict:__init(table)
 	local dict_data = {}
 	for k,v in pairs(table) do
+		-- Check dimension
+		if (type(v) == "table") then
+			for i=1,#v do
+				assert(type(v[i]) ~= "table",
+				      ("For key '%s' in the position %d the value is a table, this isn't allowed"):format(k, i))
+			end
+		end
 		dict_data[k] = v
 	end
 
--- helper_classes/tbl.lua
+++ helper_classes/tbl.lua
@@ -3,8 +3,10 @@ require 'torch'
 -- create class object
 local dtbl = torch.class('Df_Tbl')
 
+-- This is the fastes table wrapper that doesn't care to copy the original
+--  data. Should be used sparingly.
 function dtbl:__init(table)
-	self.data = clone(table)
+	self.data = table
 end
 
 return dtbl
