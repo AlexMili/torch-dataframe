417299767e97f143c2edb39b4a19efb7d786423f
Max Gordon <max@gforge.se>
2016-05-17 20:38:39 +0200

    Added argcheck to missing_data

--- Extensions/missing_data.lua
+++ Extensions/missing_data.lua
@@ -1,89 +1,116 @@
-require 'dok'
 local params = {...}
 local Dataframe = params[1]
 
---
--- count_na() : count missing values in dataset
---
--- ARGS: column_name (optional) [string] : if we only want a single columns na
---
--- RETURNS: table containing missing values per column or single integer
---
-function Dataframe:count_na(...)
-	local args = dok.unpack(
-		{...},
-		'Dataframe.count_na',
-		'Count mising values in dataset',
-		{arg='columns', type='string|table', help='the columns to count to labels'}
-	)
-	local single
-	if (args.columns == nil) then
-		args.columns = self.columns
-	elseif(type(args.columns) == 'string') then
-		single = true
-		args.columns = {args.columns}
-	else
-		error("Invalid columns argument: " .. tostring(args.columns))
-	end
-	count = {}
+local argcheck = require "argcheck"
 
-	for _,column_name in pairs(args.columns) do
-		counter = 0
-		for i = 1, self.n_rows do
-			local val = self.dataset[column_name][i]
-			if val == nil or val == '' or isnan(val) then
-				counter = counter + 1
-			end
+Dataframe.count_na = argcheck{
+	doc =  [[
+<a name="Dataframe.count_na">
+### Dataframe.count_na(@ARGP)
+
+@ARGT
+
+Count missing values in dataset
+
+_Return value_: table containing missing values per column
+]],
+	{name="self", type="Dataframe"},
+	call=function(self)
+
+	return self:count_na(Df_Array(self.column_order))
+
+end}
+
+Dataframe.count_na = argcheck{
+	doc = [[
+You can manually choose the columns by providing a Df_Array
+
+@ARGT
+
+]],
+	overload=Dataframe.count_na,
+	{name="self", type="Dataframe"},
+	{name="columns", type="Df_Array", doc="The columns to count"},
+	call=function(self, columns)
+	columns = columns.data
+
+	local ret = {}
+	for i=1,#columns do
+		ret[columns[i]] = self:count_na(columns[i])
+	end
+
+	return ret
+end}
+
+Dataframe.count_na = argcheck{
+	doc =  [[
+If you only want to count a single column
+
+@ARGT
+
+_Return value_: single integer
+	]],
+	overload=Dataframe.count_na,
+	{name="self", type="Dataframe"},
+	{name="column", type="string", doc="The column to count"},
+	call=function(self, column)
+
+	counter = 0
+	for i = 1, self.n_rows do
+		local val = self.dataset[column][i]
+		if val == nil or val == '' or isnan(val) then
+			counter = counter + 1
 		end
-		count[column_name] = counter
 	end
 
-	if (single) then
-		return count[args.columns[1]]
-	else
-		return count
-	end
-end
+	return counter
+end}
 
---
--- fill_na('column_name', 0) : replace missing value in a specific column
---
--- ARGS: - column_name 		(required) 				[string]	: column name to fill
---		 - default_value 	(optional, default=0) 	[any]		: default missing value
---
--- RETURNS: nothing
---
--- Enhancement : detect nil/na value at first reading or _infer_schema
-function Dataframe:fill_na(column_name, default_value)
+Dataframe.fill_na = argcheck{
+	doc = [[
+<a name="Dataframe.fill_na">
+### Dataframe.fill_na(@ARGP)
+
+Replace missing value in a specific column
+
+@ARGT
+
+_Return value_: void
+]],
+	{name="self", type="Dataframe"},
+	{name="column_name", type="string", doc="The column to fill"},
+	{name="default_value", type="number|string|boolean", doc="The default missing value", default=0},
+	call=function(self, column_name, default_value)
 	assert(self:has_column(column_name), "Could not find column: " .. tostring(column_name))
-	if (self:count_na(column_name) == 0) then
-		return
-	end
 
-	default_value = default_value or 0
-  if (self:is_categorical(column_name) and
-      self.categorical[column_name][default_value] == nil) then
-    self.categorical[column_name]["__nan__"] = default_value
-  end
+	if (self:is_categorical(column_name) and
+	    self.categorical[column_name][default_value] == nil) then
+		self.categorical[column_name]["__nan__"] = default_value
+	end
 
 	for i = 1, self.n_rows do
 		local val = self.dataset[column_name][i]
-		if val == nil or val == '' or isnan(val) then
+		if val == nil or isnan(val) then
 			self.dataset[column_name][i] = default_value
 		end
 	end
-end
+end}
 
---
--- fill_all_na(0) : replace missing value in the whole dataset
---
--- ARGS: - default_value (optional, default=0) [any] : default missing value
---
--- RETURNS: nothing
---
--- Enhancement : detect nil/na value at first reading or _infer_schema
-function Dataframe:fill_all_na(default_value)
+Dataframe.fill_all_na = argcheck{
+	doc = [[
+<a name="Dataframe.fill_na">
+### Dataframe.fill_na(@ARGP)
+
+Replace missing value in all columns
+
+@ARGT
+
+_Return value_: void
+]],
+	{name="self", type="Dataframe"},
+	{name="default_value", type="number|string|boolean", doc="The default missing value", default=0},
+	call=function(self, default_value)
 	for _,key in pairs(self.columns) do
 		self:fill_na(key, default_value)
 	end
-end
+end}
