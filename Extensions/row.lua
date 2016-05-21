a5707baeba03bee9740da7c1aebc46ab85532b47
Max Gordon <max@gforge.se>
2016-05-21 12:24:07 +0200

    Added append and updated insert to use index

--- Extensions/row.lua
+++ Extensions/row.lua
@@ -45,14 +45,138 @@ Dataframe.insert = argcheck{
 <a name="Dataframe.insert">
 ### Dataframe.insert(@ARGP)
 
-Inserts a row or multiple rows into database. Automatically appends to the Dataframe.
+Inserts a row or multiple rows into database at the position of the provided index.
 
 @ARGT
 
 _Return value_: void
 ]],
 	{name="self", type="Dataframe"},
+	{name="index", type="number", doc="The row number where to insert the row(s)"},
 	{name="rows", type="Df_Dict", doc="Insert values to the dataset"},
+	call=function(self, index, rows)
+	if (self:size(1) == 0 and index == 1) then
+		return self:append{rows = rows}
+	end
+
+	assert(isint(index), "The index should be an integer, you've provided " .. tostring(index))
+	assert(index > 0 and index <= self.n_rows, ("The index (%d) is outside the bounds 1-%d"):format(index, self.n_rows))
+
+	rows = self:_check_and_prep_row_argmnt{rows = rows,
+		add_new_columns = true,
+		add_old_columns = true}
+
+	for _, column_name in pairs(self.columns) do
+		for j = index,(index + no_rows_2_insert) do
+			value = rows[column_name][j]
+			if (self:is_categorical(column_name) and
+			    not isnan(value)) then
+				vale = self:_get_raw_cat_key(column_name, value)
+			end -- TODO: Should we convert string columns with '' to nan?
+			table.insert(self.dataset[column_name], j, value)
+		end
+	end
+
+	self:_refresh_metadata()
+	self:_infer_schema()
+end}
+
+Dataframe.insert = argcheck{
+	doc =  [[
+Note, if you provide a Dataframe the primary dataframes meta-information will
+be the ones that are kept
+
+@ARGT
+
+]],
+	overload=Dataframe.insert,
+	{name="self", type="Dataframe"},
+	{name="index", type="number", doc="The row number where to insert the row(s)"},
+	{name="rows", type="Dataframe", doc="A Dataframe that you want to insert"},
+	call=function(self, index, rows)
+	return self:insert(index, Df_Dict(rows.dataset))
+end}
+
+Dataframe.insert = argcheck{
+	overload=Dataframe.insert,
+	{name="self", type="Dataframe"},
+	{name="rows", type="Df_Dict", doc="Insert values to the dataset"},
+	call=function(self, rows)
+	print("Warning: The insert without row number is a deprecated function and support will be dropped - use append instead")
+	return self:append(rows)
+end}
+
+Dataframe.insert = argcheck{
+	overload=Dataframe.insert,
+	{name="self", type="Dataframe"},
+	{name="rows", type="Dataframe", doc="A Dataframe that you want to append"},
+	call=function(self, rows)
+		print("Warning: The insert without row number is a deprecated function and support will be dropped - use append instead")
+	self:append(Df_Dict(rows.dataset))
+end}
+
+Dataframe._check_and_prep_row_argmnt  = argcheck{
+	{name="self", type="Dataframe"},
+	{name="rows", type="Df_Dict"},
+	{name="add_new_columns", type="boolean", default=true,
+	 doc="Add columns with missing values to the datafame that appear in the rows dict"},
+	{name="add_old_columns", type="boolean", default=true,
+	 doc="Add columns with missing values to rows that are only in the dataframe"},
+	call=function(self, rows, add_new_columns, add_old_columns)
+	rows = rows.data
+	local no_rows_2_insert = 0
+	local new_columns = {}
+	for k,v in pairs(rows) do
+		-- Force all input into tables
+		if (type(v) ~= 'table') then
+			v = {v}
+			rows[k] = v
+		end
+
+		-- Check input size
+		if (no_rows_2_insert == 0) then
+			no_rows_2_insert = table.maxn(v)
+		else
+			assert(no_rows_2_insert == table.maxn(v),
+						 "The rows aren't the same between the columns." ..
+						 " The " .. k .. " column has " .. " " .. table.maxn(v) .. " rows" ..
+						 " while previous columns had " .. no_rows_2_insert .. " rows")
+		end
+
+		if (not table.has_element(self.columns, k) and add_new_columns) then
+			self:add_column(k)
+		end
+	end
+
+	if (add_old_columns) then
+		for i=1,#self.columns do
+			local k = self.columns[i]
+			if (rows[k] == nil) then
+				local tmp = {}
+				for i=1,no_rows_2_insert do
+					tmp[i] = 0/0
+				end
+				rows[k] = tmp
+			end
+		end
+	end
+
+	return rows
+end}
+
+Dataframe.append = argcheck{
+	doc =  [[
+<a name="Dataframe.append">
+### Dataframe.append(@ARGP)
+
+Appends the row(s) to the Dataframe.
+
+@ARGT
+
+_Return value_: void
+]],
+	{name="self", type="Dataframe"},
+	{name="rows", type="Df_Dict", doc="Values to append to the Dataframe"},
 	call=function(self, rows)
 	rows = rows.data
 	if (self:size(1) == 0) then
@@ -106,6 +230,21 @@ _Return value_: void
 	self:_infer_schema()
 end}
 
+Dataframe.append = argcheck{
+	doc =  [[
+Note, if you provide a Dataframe the primary dataframes meta-information will
+be the ones that are kept
+
+@ARGT
+
+]],
+	overload=Dataframe.append,
+	{name="self", type="Dataframe"},
+	{name="rows", type="Dataframe", doc="A Dataframe that you want to append"},
+	call=function(self, rows)
+	self:append(Df_Dict(rows.dataset))
+end}
+
 Dataframe.remove_index = argcheck{
 	doc =  [[
 <a name="Dataframe.remove_index">
@@ -129,22 +268,4 @@ _Return value_: void
 	self.n_rows = self.n_rows - 1
 
 	self:_refresh_metadata()
-end}
-
-Dataframe.insert = argcheck{
-	doc =  [[
-<a name="Dataframe.insert">
-### Dataframe.insert(@ARGP)
-
-Inserts a row or multiple rows into database. Automatically appends to the Dataframe.
-
-@ARGT
-
-_Return value_: void
-]],
-	{name="self", type="Dataframe"},
-	{name="index", type="number", doc="Insert values to the dataset"},
-	{name="rows", type="Df_Dict", doc="Insert values to the dataset"},
-	call=function(self, index, rows)
-
 end}
