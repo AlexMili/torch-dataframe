f4a580890248fd5daaeb74a089fe8c21a8954148
Max Gordon <max@gforge.se>
2016-05-28 05:38:03 +0200

    Added metatable extension

---
+++ Extensions/metatable.lua
@@ -0,0 +1,98 @@
+local params = {...}
+local Dataframe = params[1]
+
+local argcheck = require "argcheck"
+local doc = require "argcheck.doc"
+
+doc[[
+
+## Metatable functions
+
+]]
+
+Dataframe.size = argcheck{
+	doc =  [[
+<a name="Dataframe.size">
+### Dataframe.size(@ARGP)
+
+Returns the number of rows and columns in a tensor
+
+@ARGT
+
+_Return value_: tensor (rows, columns)
+]],
+	{name="self", type="Dataframe"},
+	call=function(self)
+	return torch.IntTensor({self.n_rows,#self.columns})
+end}
+
+Dataframe.size = argcheck{
+	doc =  [[
+By providing dimension you can get only that dimension, row == 1, col == 2
+
+@ARGT
+
+_Return value_: integer
+]],
+	overload=Dataframe.size,
+	{name="self", type="Dataframe"},
+	{name="dim", type="number", doc="The dimension of interest"},
+	call=function(self, dim)
+	assert(isint(dim), "The dimension isn't an integer: " .. tostring(dim))
+	assert(dim == 1 or dim == 2, "The dimension can only be between 1 and 2 - you've provided: " .. dim)
+	if (dim == 1) then
+		return self.n_rows
+	end
+
+	return #self.columns
+end}
+
+-- Wait until https://github.com/torch/torch7/issues/693 is resolved
+if (false) then
+doc =  [[
+<a name="Dataframe.[]">
+### Dataframe.[]
+
+Subsets rows. You can either use integers corresponding to valid row numbers,
+or you can provide a Df_Array() with multiple values or you can provide a
+row span "2:5"
+
+_Return value_: Dataframe
+]]
+
+function Dataframe:__index__(index)
+	if (type(index) == "number") then
+		local tmp = self:_create_subset(Df_Array(index))
+		print(torch.type(tmp))
+		return tmp
+	end
+
+	if (false and type(index) == "string") then
+		if (index:match("^[0-9]+:[0-9]+$")) then
+			-- Get the core data
+			local first = index:gsub(":.*", "")
+			first = tonumber(first)
+			local last = index:gsub("[^:]+:", "")
+			last = tonumber(last)
+
+			local indexes = {}
+			if (first <= last) then
+				for i=first,last do
+					table.insert(indexes, i)
+				end
+			else
+				-- insert in reverse order
+				for i=last,first do
+					table.insert(indexes, 1, i)
+				end
+			end
+
+			return self:_create_subset(Df_Array(indexes))
+		end
+
+		return rawget(self, index)
+	end
+
+	return rawget(self, index)
+end
+end
---
+++ specs/metatble_specs.lua
@@ -0,0 +1,81 @@
+require 'lfs'
+require 'torch'
+
+-- Make sure that directory structure is always the same
+if (string.match(lfs.currentdir(), "/specs$")) then
+  lfs.chdir("..")
+end
+
+-- Include Dataframe lib
+paths.dofile('init.lua')
+
+-- Go into specs so that the loading of CSV:s is the same as always
+lfs.chdir("specs")
+
+describe("Indexing the dataframe", function()
+
+	describe("Retrieving index",function()
+		local df = Dataframe("./data/simple_short.csv")
+		-- Wait until https://github.com/torch/torch7/issues/693 is resolved
+		if (false) then
+		it("Retrieves a single row",function()
+			local subset = df[1]
+			print(torch.type(subset))
+			assert.is.truthy(subset, "Fails to subset row")
+			assert.are.same(subset:size(1), 1)
+		end)
+
+		it("Retrieves a several rows",function()
+			local subset = df[Df_Array(1, 3)]
+			assert.is.truthy(subset, "Fails to subset rows")
+			assert.are.same(subset:size(1), 2)
+			assert.are.same(subset:size(2), df:size(2))
+		end)
+
+		it("Retrieves a continuous set of rows",function()
+			local subset = df["1:4"]
+			assert.is.truthy(subset, "Fails to subset rows with continuous syntax")
+			assert.are.same(subset:size(1), 4)
+			assert.are.same(subset:size(2), df:size(2))
+		end)
+		end
+	end)
+
+	describe("Set row via the newindex",function()
+		local df = Dataframe()
+
+		it("Set a single row",function()
+		end)
+
+		it("Set multiple rows",function()
+		end)
+	end)
+
+	describe("Create a copy of the table",function()
+		local df = Dataframe()
+
+		it("Set a single row",function()
+		end)
+
+		it("Set multiple rows",function()
+		end)
+	end)
+
+	it("Returns the size of the Dataframe",function()
+		local a = Dataframe(Df_Dict({test = {1,nil,3, 4}, test2 = {5, 9, 99, 88}}))
+
+		assert.is_true(torch.all(torch.eq(a:size(), torch.IntTensor({4, 2}))))
+		assert.are.same(a:size(1), 4)
+		assert.are.same(a:size(2), 2)
+	end)
+
+	describe("Gets the version number",function()
+		local df = Dataframe()
+
+		it("The torch.version goes to version()",function()
+			assert.are.same(torch.version(df), df:version())
+		end)
+
+	end)
+
+end)
