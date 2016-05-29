a28950fd76615e388fbd0f6cae50972d8c8fc006
Max Gordon <max@gforge.se>
2016-05-29 23:03:15 +0200

    Changed load_batch to use named argument

--- specs/load_batch_spec.lua
+++ specs/load_batch_spec.lua
@@ -13,16 +13,18 @@ paths.dofile('init.lua')
 lfs.chdir("specs")
 
 describe("Loading batch process", function()
+	local fake_loader = function(row) return torch.Tensor({1, 2}) end
 	local a = Dataframe("./data/realistic_29_row_data.csv")
 
 	it("Raises an error if init_batch hasn't be called",function()
-		assert.has.error(function() a:load_batch() end)
+		assert.has.error(function() a:load_batch{no_lines = 10,
+		                                         load_row_fn = fake_loader} end)
 	end)
 
 	it("Initializes",function()
 		torch.manualSeed(0)
 		a:init_batch()
-		order = 0
+		local order = 0
 
 		for i = 2,#a.batch.datasets["train"] do
 			order = order + a.batch.datasets["train"][i] - a.batch.datasets["train"][i - 1] - 1
@@ -56,7 +58,10 @@ describe("Loading batch process", function()
 
 		it("Loads batches",function()
 			a:init_batch()
-			data, label = a:load_batch(5, 0, function(row) return torch.Tensor({1, 2}) end, 'train')
+			local data, label =
+				a:load_batch{no_lines = 5,
+				             load_row_fn = fake_loader,
+				             type = 'train'}
 			assert.is.equal(data:size(1), 5)-- "The data has invalid rows"
 			assert.is.equal(data:size(2), 2)-- "The data has invalid columns"
 			assert.is.equal(label:size(1), 5)--"The labels have invalid size"
@@ -64,13 +69,22 @@ describe("Loading batch process", function()
 
 		it("Loads categorical columns",function()
 			a:as_categorical('Gender')
-			data, label, names = a:load_batch(5, 0, function(row) return torch.Tensor({1, 2}) end,'train')
+			local data, label, names =
+				a:load_batch{no_lines = 5,
+				             load_row_fn = fake_loader,
+				             type = 'train',
+				             offset = 0}
 			assert.is.equal(data:size(1), 5)-- "The data with gender has invalid rows"
 			assert.is.equal(data:size(2), 2)-- "The data with gender has invalid columns"
 			assert.is.equal(label:size(1), 5)-- "The labels with gender have invalid size"
 			assert.are.same(names, {'Gender', 'Weight'})-- "Invalid names returned"
 
-			data, label = a:load_batch(5, 0,function(row) return torch.Tensor({1, 2}) end,'train')
+			data, label, names =
+				a:load_batch{no_lines = 5,
+				             load_row_fn = fake_loader,
+				             type = 'train',
+				             offset = 0}
+
 			assert.is.equal(data:size(1), 5)-- "The data with gender has invalid rows"
 			assert.is.equal(data:size(2), 2)-- "The data with gender has invalid columns"
 			assert.is.equal(label:size(1), 5)-- "The labels with gender have invalid size"
@@ -79,23 +93,51 @@ describe("Loading batch process", function()
 		it("Doesn't load all cases",function()
 			local batch_size = 6
 			for i=1,10 do
-				data, label = a:load_batch(batch_size, (i - 1)*batch_size,function(row) return torch.Tensor({1, 2}) end,'train')
+				local data, label, names =
+					a:load_batch{no_lines = batch_size,
+					             load_row_fn = fake_loader,
+					             type = 'train',
+					             offset = (i - 1)*batch_size}
 				assert.is.equal(label:size(1), batch_size)-- "The labels have invalid size at iteration " .. i
 				assert.is.equal(data:size(1), batch_size)-- "The data has invalid size at iteration " .. i
 			end
 
-			data, label = a:load_batch(-1, 0,function(row) return torch.Tensor({1, 2}) end,'train')
+			local data, label =
+				a:load_batch{no_lines = -1,
+				            load_row_fn = fake_loader,
+				            offset = 0,
+				            type = 'train'}
 			assert.are.same(data:size(1), a:batch_size('train'))-- "Doesn't load all train cases"
-			data, label = a:load_batch(-1, 0,function(row) return torch.Tensor({1, 2}) end,'validate')
+			data, label =
+				a:load_batch{no_lines = -1,
+				            load_row_fn = fake_loader,
+				            offset = 0,
+				            type = 'validate'}
 			assert.are.same(data:size(1), a:batch_size('validate'))-- "Doesn't load all validation cases"
-			data, label = a:load_batch(-1, 0,function(row) return torch.Tensor({1, 2}) end,'test')
+			data, label =
+				a:load_batch{no_lines = -1,
+				            load_row_fn = fake_loader,
+				            offset = 0,
+				            type = 'test'}
 			assert.are.same(data:size(1), a:batch_size('test'))-- "Doesn't load all test cases"
 
-			data, label = a:load_batch(a:batch_size('train'), 0,function(row) return torch.Tensor({1, 2}) end,'train')
+			data, label =
+				a:load_batch{no_lines = a:batch_size('train'),
+				            load_row_fn = fake_loader,
+				            offset = 0,
+				            type = 'train'}
 			assert.are.same(data:size(1), a:batch_size('train'))-- "Doesn't load all train cases when max number specified"
-			data, label = a:load_batch(a:batch_size('validate'), 0,function(row) return torch.Tensor({1, 2}) end,'validate')
+			data, label =
+				a:load_batch{no_lines = a:batch_size('validate'),
+				            load_row_fn = fake_loader,
+				            offset = 0,
+				            type = 'validate'}
 			assert.are.same(data:size(1), a:batch_size('validate'))-- "Doesn't load all validation cases when max number specified"
-			data, label = a:load_batch(a:batch_size('test'), 0,function(row) return torch.Tensor({1, 2}) end,'test')
+			data, label =
+				a:load_batch{no_lines = a:batch_size('test'),
+				            load_row_fn = fake_loader,
+				            offset = 0,
+				            type = 'test'}
 			assert.are.same(data:size(1), a:batch_size('test'))-- "Doesn't load all test cases when max number specified"
 		end)
 	end)
