c40f99c592e060c076b3caab7b81e73c89274e47
Max Gordon <max@gforge.se>
2016-06-04 21:34:24 +0200

    Changd to busted test structure Added csv-files that torch-dataset tests use

--- specs/sampler_spec.lua
+++ specs/sampler_spec.lua
@@ -16,8 +16,8 @@ paths.dofile('init.lua')
 -- Go into specs so that the loading of CSV:s is the same as always
 lfs.chdir("specs")
 
-testPermutationSampler = function()
-	local index = IndexCSV(paths.concat(paths.dirname(paths.thisfile()), 'index.csv'))
+describe("Permutation Sampler", function()
+	local index = IndexCSV('./data/sampler_csv_files/index.csv')
 	local sampler,resetSampler = Sampler('permutation', index)
 	local seen = { }
 	local labelCounts = { }
@@ -31,20 +31,20 @@ testPermutationSampler = function()
 			labelCounts[label] = labelCounts[label] + 1
 		end
 		if i % 25 == 0 then
-			test.mustBeTrue(sampler() == nil, 'going past the end must return nil')
+			assert.is_true(sampler() == nil, 'going past the end must return nil')
 			resetSampler()
 			if prev ~= nil then
-				test.mustBeTrue(#seen == #prev, 'need to see the same amount sampled each loop')
-				test.mustBeTrue(TestUtils.listEquals(seen, prev) == false, 'the lists must have different orders')
+				assert.are.same(#seen, #prev, 'need to see the same amount sampled each loop')
+				assert.are_not.same(seen, prev, 'the lists must have different orders')
 				local sortedSeen = TestUtils.listCopy(seen)
 				table.sort(sortedSeen)
 				local sortedPrev = TestUtils.listCopy(prev)
 				table.sort(sortedPrev)
-				test.mustBeTrue(TestUtils.listEquals(sortedSeen, sortedPrev) == true, 'the lists must have the same sorted orders')
+				assert.are.same(sortedSeen, sortedPrev, 'the lists must have the same sorted orders')
 				for _,label in ipairs(index.labels) do
 					local x = index.itemCount(label)
 					local y = labelCounts[label]
-					test.mustBeTrue(x == y, 'must have the same distribution of '..label..' saw '..y..' expected '..x)
+					assert.are.same(x, y, 'must have the same distribution of '..label..' saw '..y..' expected '..x)
 				end
 			end
 			prev = seen
@@ -52,10 +52,10 @@ testPermutationSampler = function()
 			labelCounts = { }
 		end
 	end
-end
+end)
 
-testLabelPermutationSampler = function()
-	local index = IndexCSV(paths.concat(paths.dirname(paths.thisfile()), 'index.csv'))
+describe("Label Permutation Sampler", function()
+	local index = IndexCSV('./data/sampler_csv_files/index.csv')
 	local sampler = Sampler('label-permutation', index)
 	local seen = { }
 	local seenClasses = { }
@@ -75,18 +75,18 @@ testLabelPermutationSampler = function()
 
 		if i % 3 == 0 then
 			if prevClasses ~= nil then
-				test.mustBeTrue(#seenClasses == #prevClasses, 'need to see the same amount sampled each loop')
-				test.mustBeTrue(TestUtils.listEquals(seenClasses, prevClasses) == false, 'the lists must have different orders')
+				assert.are.same(#seenClasses, #prevClasses, 'need to see the same amount sampled each loop')
+				assert.are_not.same(seenClasses, prevClasses, 'the lists must have different orders')
 				local sortedSeen = TestUtils.listCopy(seenClasses)
 				table.sort(sortedSeen)
 				local sortedPrev = TestUtils.listCopy(prevClasses)
 				table.sort(sortedPrev)
-				test.mustBeTrue(TestUtils.listEquals(sortedSeen, sortedPrev) == true, 'the lists must have the same sorted orders')
+				assert.are.same(sortedSeen, sortedPrev, 'the lists must have the same sorted orders')
 
 				for _,label in ipairs(index.labels) do
 					local x = 1
 					local y = labelCountsClasses[label]
-					test.mustBeTrue(x == y, 'wrong distribution saw '..y..' expected '..x)
+					assert.are.same(x, y, 'wrong distribution saw '..y..' expected '..x)
 				end
 			end
 			prevClasses = seenClasses
@@ -96,17 +96,17 @@ testLabelPermutationSampler = function()
 
 		if i % fullLoop == 0 then
 			if prev ~= nil then
-				test.mustBeTrue(#seen == #prev, 'need to see the same amount sampled each loop')
-				test.mustBeTrue(TestUtils.listEquals(seen, prev) == false, 'the lists must have different orders')
+				assert.are.same(#seen, #prev, 'need to see the same amount sampled each loop')
+				assert.are_not.same(seen, prev, 'the lists must have different orders')
 				local sortedSeen = TestUtils.listCopy(seen)
 				table.sort(sortedSeen)
 				local sortedPrev = TestUtils.listCopy(prev)
 				table.sort(sortedPrev)
-				test.mustBeTrue(TestUtils.listEquals(sortedSeen, sortedPrev) == true, 'the lists must have the same sorted orders')
+				assert.are.same(sortedSeen, sortedPrev, 'the lists must have the same sorted orders')
 				for _,label in ipairs(index.labels) do
 					local x = fullLoop / #index.labels
 					local y = labelCounts[label]
-					test.mustBeTrue(x == y, 'must have the same distribution of '..label..' saw '..y..' expected '..x)
+					assert.are.same(x, y, 'must have the same distribution of '..label..' saw '..y..' expected '..x)
 				end
 			end
 			prev = seen
@@ -114,52 +114,52 @@ testLabelPermutationSampler = function()
 			labelCounts = { }
 		end
 	end
-end
+end)
 
-testPermutationSamplerWithLabel = function()
-	local index = IndexCSV(paths.concat(paths.dirname(paths.thisfile()), 'index.csv'))
+describe("Permutation Sampler With Label", function()
+	local index = IndexCSV('./data/sampler_csv_files/index.csv')
 	local sampler,resetSampler = Sampler('permutation', index, 'B')
 	local seen = { }
 	local prev
 	for i = 1,27 do
 		local s,label = sampler()
-		test.mustBeTrue(label == 'B', 'label must always be B')
+		assert.are.same(label, 'B', 'label must always be B')
 		table.insert(seen, s)
 		if i % 9 == 0 then
-			test.mustBeTrue(sampler() == nil, 'going past the end must return nil')
+			assert.are.same(sampler(), nil, 'going past the end must return nil')
 			resetSampler()
 			if prev ~= nil then
-				test.mustBeTrue(#seen == #prev, 'need to see the same amount sampled each loop')
-				test.mustBeTrue(TestUtils.noDupes(seen) == true, 'should not see any dupes')
-				test.mustBeTrue(TestUtils.listEquals(seen, prev) == false, 'the lists must have different orders')
+				assert.are.same(#seen, #prev, 'need to see the same amount sampled each loop')
+				assert.is_true(TestUtils.noDupes(seen) == true, 'should not see any dupes')
+				assert.are_not.same(seen, prev, 'the lists must have different orders')
 				local sortedSeen = TestUtils.listCopy(seen)
 				table.sort(sortedSeen)
 				local sortedPrev = TestUtils.listCopy(prev)
 				table.sort(sortedPrev)
-				test.mustBeTrue(TestUtils.listEquals(sortedSeen, sortedPrev) == true, 'the lists must have the same sorted orders')
+				assert.are.same(sortedSeen, sortedPrev, 'the lists must have the same sorted orders')
 			end
 			prev = seen
 			seen = { }
 		end
 	end
-end
+end)
 
-testLinearSampler = function()
-	local index = IndexCSV(paths.concat(paths.dirname(paths.thisfile()), 'index.csv'))
+describe("Linear Sampler", function()
+	local index = IndexCSV('./data/sampler_csv_files/index.csv')
 	local sampler,resetSampler = Sampler('linear', index, 'B')
 	for j = 1,3 do
 		for i = 1,index.itemCount('B') do
 			local s,label = sampler()
-			test.mustBeTrue(label == 'B', 'label must always be B')
-			test.mustBeTrue(s == index.itemAt(i, 'B'), 'must sample in index order')
+			assert.are.same(label, 'B', 'label must always be B')
+			assert.are.same(s, index.itemAt(i, 'B'), 'must sample in index order')
 		end
-		test.mustBeTrue(sampler() == nil, 'going past the end must return nil')
+		assert.is_true(sampler() == nil, 'going past the end must return nil')
 		resetSampler()
 	end
-end
+end)
 
-testLabelUniformSampler = function()
-	local index = IndexCSV(paths.concat(paths.dirname(paths.thisfile()), 'index.csv'))
+describe("Label Uniform Sampler", function()
+	local index = IndexCSV('./data/sampler_csv_files/index.csv')
 	local sample = Sampler('label-uniform', index)
 	local hist = {}
 	for i = 1,1e6 do
@@ -167,13 +167,13 @@ testLabelUniformSampler = function()
 		hist[label] = (hist[label] or 0) + 1
 	end
 	local ratioA = math.abs((3 / (1e6/hist.A)) - 1)
-	test.mustBeTrue(ratioA < .1, 'ratios of labels A must be 1/3')
+	assert.is_true(ratioA < .1, 'ratios of labels A must be 1/3')
 	local ratioB = math.abs((3 / (1e6/hist.A)) - 1)
-	test.mustBeTrue(ratioB < .1, 'ratios of labels B must be 1/3')
-end
+	assert.is_true(ratioB < .1, 'ratios of labels B must be 1/3')
+end)
 
-testUniformSampler = function()
-	local index = IndexCSV(paths.concat(paths.dirname(paths.thisfile()), 'index3.csv'))
+describe("Uniform Sampler", function()
+	local index = IndexCSV('./data/sampler_csv_files/index3.csv')
 	local sample = Sampler('uniform', index)
 	local hist = {}
 	for i = 1,1e6 do
@@ -182,5 +182,5 @@ testUniformSampler = function()
 	end
 	hist = torch.Tensor(hist):div(1e6/index.itemCount()):add(-1):abs()
 	local err = hist:max()
-	test.mustBeTrue(err < .1, 'ratios of samples must be uniform')
-end
+	assert.is_true(err < .1, 'ratios of samples must be uniform')
+end)
