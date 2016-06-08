c6c275ccb103d02f0639745a71e2b6a0aca8e129
Max Gordon <max@gforge.se>
2016-06-08 23:25:49 +0200

    Switched from type to torch.type This allows Df_Array/Df_Dict/Df_Tbl to pass

--- helper_classes/dict.lua
+++ helper_classes/dict.lua
@@ -7,7 +7,7 @@ function dict:__init(table)
 	local dict_data = {}
 	for k,v in pairs(table) do
 		-- Check dimension
-		if (type(v) == "table") then
+		if (torch.type(v) == "table") then
 			for i=1,#v do
 				assert(type(v[i]) ~= "table",
 				      ("For key '%s' in the position %d the value is a table, this isn't allowed"):format(k, i))
