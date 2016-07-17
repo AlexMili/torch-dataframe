4dc281e0b47a01560d373901f0518d364d7f760f
Max Gordon <max@gforge.se>
2016-07-17 22:53:32 +0200

    Indentation change

--- helper_classes/paralleliterator.lua
+++ helper_classes/paralleliterator.lua
@@ -13,7 +13,7 @@ local Threads = require 'threads'
 local argcheck = require 'argcheck'
 local doc = require 'argcheck.doc'
 
-local Df_ParallelIterator = torch.class('Df_ParallelIterator', 'tnt.DatasetIterator')
+local Df_ParallelIterator, parent_class = torch.class('Df_ParallelIterator', 'Df_Iterator')
 
 Df_ParallelIterator.__init = argcheck{
 	doc = [[
@@ -42,7 +42,7 @@ For more information, check out the [threads package](https://github.com/torch/t
 on which `Df_ParallelIterator` relies.
 ]],
 	{name='self', type='Df_ParallelIterator'},
-	{name='data', type='Df_Subset', doc='The Dataframe subset to use for the iterator'},
+	{name='dataset', type='Df_Subset', doc='The Dataframe subset to use for the iterator'},
 	{name="batch_size", type="number", doc="The size of the batches"},
 	{name='init', type='function', default=function(idx)
 		-- Load the libraries needed
@@ -69,163 +69,162 @@ on which `Df_ParallelIterator` relies.
 	 By default `ordered` is false, which means that order is not guaranteed by
 	 `run()` (though often the ordering is similar in practice).]]},
 	call =
-	function(self, data, batch_size, init, nthread,
+	function(self, dataset, batch_size, init, nthread,
 		       filter, transform, input_transform, target_transform, ordered)
-		assert(isint(batch_size) and batch_size > 0, "The batch size must be a positive integer")
-		assert(data.batch_args,
-		       "If you want to use the iterator you must prespecify the batch data/label loaders")
+	assert(isint(batch_size) and batch_size > 0, "The batch size must be a positive integer")
+	assert(dataset.batch_args,
+	       "If you want to use the iterator you must prespecify the batch data/label loaders")
 
-		-- The sharing allows shared access to tds/tensors
-		Threads.serialization('threads.sharedserialize')
+	-- The sharing allows shared access to tds/tensors
+	Threads.serialization('threads.sharedserialize')
 
-		-- Initialize the threads and the environement
-		local threads = Threads(nthread, init)
-		self.__threads = threads
-		self.__nthread = nthread
+	-- Initialize the threads and the environement
+	local threads = Threads(nthread, init)
+	self.__threads = threads
+	self.__nthread = nthread
 
-		self.dataset = data
+	self.dataset = dataset
 
+	-- The size should be the number of batches that will be performed
+	local size = math.ceil(self:exec("size")/batch_size)
 
-		-- The size should be the number of batches that will be performed
-		local size = math.ceil(data:size()/batch_size)
+	local sample -- beware: do not put this line in loop()
+	local sampleOrigIdx
 
-		local sample -- beware: do not put this line in loop()
-		local sampleOrigIdx
+	function self.run()
+		-- loading size of the dataset each time run() is called
+		threads:addjob(
+			function(argList)
+				-- make the mod/filter functions global
+				filter, transform, input_transform, target_transform = unpack(argList)
+			end,
+			function()
+			end,
+			{filter, transform, input_transform, target_transform}
+		)
+		threads:dojob()
 
-		function self.run()
-			-- loading size of the dataset each time run() is called
-			threads:addjob(
-				function(argList)
-					-- make the mod/filter functions global
-					filter, transform, input_transform, target_transform = unpack(argList)
-				end,
-				function()
-				end,
-				{filter, transform, input_transform, target_transform}
-			)
-			threads:dojob()
+		-- `samplePlaceholder` stands in for samples which have been
+		-- filtered out by the `filter` function
+		local samplePlaceholder = {}
 
-			-- `samplePlaceholder` stands in for samples which have been
-			-- filtered out by the `filter` function
-			local samplePlaceholder = {}
+		-- The enque does the main loop
+		local idx = 1
+		local function enqueue()
+			while idx <= size and threads:acceptsjob() do
+				local batch, reset = self.dataset:get_batch(batch_size)
 
-			-- The enque does the main loop
-			local idx = 1
-			local function enqueue()
-				while idx <= size and threads:acceptsjob() do
-					local batch, reset = self.dataset:get_batch(batch_size)
+				if (reset) then
+					idx = size + 1
+				else
+					idx = idx + 1
+				end
 
-					if (reset) then
-						idx = size + 1
-					else
-						idx = idx + 1
-					end
+				if (batch) then
+					-- TODO: the retriever functions can be unexpectedly large see http://stackoverflow.com/questions/38421366/torch-out-of-memory-in-thread-when-using-torch-serialize-twice/
+					local serialized_batch = torch.serialize(batch)
 
-					if (batch) then
-						-- TODO: the retriever functions can be unexpectedly large see http://stackoverflow.com/questions/38421366/torch-out-of-memory-in-thread-when-using-torch-serialize-twice/
-						local serialized_batch = torch.serialize(batch)
+					-- In the parallel section only the to_tensor is run in parallel
+					--  this should though be the computationally expensive operation
+					threads:addjob(
+						function(argList)
+							local origIdx, serialized_batch, samplePlaceholder = unpack(argList)
 
-						-- In the parallel section only the to_tensor is run in parallel
-						--  this should though be the computationally expensive operation
-						threads:addjob(
-							function(argList)
-								local origIdx, serialized_batch, samplePlaceholder = unpack(argList)
+							local batch = torch.deserialize(serialized_batch)
 
-								local batch = torch.deserialize(serialized_batch)
+							batch = transform(batch)
 
-								batch = transform(batch)
+							local sample = samplePlaceholder
+							if (filter(batch)) then
+								sample = {}
+								sample.input, sample.target = batch:to_tensor()
+								sample.input = input_transform(sample.input)
+								sample.target = target_transform(sample.target)
+							end
 
-								local sample = samplePlaceholder
-								if (filter(batch)) then
-									sample = {}
-									sample.input, sample.target = batch:to_tensor()
-									sample.input = input_transform(sample.input)
-									sample.target = target_transform(sample.target)
-								end
+							collectgarbage()
+							collectgarbage()
 
-								collectgarbage()
-								collectgarbage()
+							return {
+								sample,
+								origIdx
+							}
+						end,
+						function(argList)
+							sample, sampleOrigIdx = unpack(argList)
+						end,
+						{idx, serialized_batch, samplePlaceholder}
+					)
 
-								return {
-									sample,
-									origIdx
-								}
-							end,
-							function(argList)
-								sample, sampleOrigIdx = unpack(argList)
-							end,
-							{idx, serialized_batch, samplePlaceholder}
-						)
+					-- The serialized_batch takes a lot of memory and needs to be destroyed
+					serialized_batch = nil
+					collectgarbage()
+					collectgarbage()
+				end
+			end
+		end
 
-						-- The serialized_batch takes a lot of memory and needs to be destroyed
-						serialized_batch = nil
-						collectgarbage()
-						collectgarbage()
-					end
+		enqueue()
+
+		local iterFunction
+		if ordered then
+			local curSampleIdx = 1
+			local storedSamples = {}
+
+			-- Move past placeholders (filtered out samples) in
+			-- `storedSamples`
+			local function advancePastPlaceholders()
+				while table.exact_length(storedSamples[curSampleIdx]) == 0 do
+					storedSamples[curSampleIdx] = nil
+					curSampleIdx = curSampleIdx + 1
 				end
 			end
 
-			enqueue()
+			iterFunction = function()
+				advancePastPlaceholders()
 
-			local iterFunction
-			if ordered then
-				local curSampleIdx = 1
-				local storedSamples = {}
-
-				-- Move past placeholders (filtered out samples) in
-				-- `storedSamples`
-				local function advancePastPlaceholders()
-					while table.exact_length(storedSamples[curSampleIdx]) == 0 do
-						storedSamples[curSampleIdx] = nil
-						curSampleIdx = curSampleIdx + 1
+				-- Load into storedSamples until we find the next sample in
+				-- the sequence or we run out of samples
+				while storedSamples[curSampleIdx] == nil and threads:hasjob() do
+					enqueue()
+					threads:dojob()
+					if threads:haserror() then
+						threads:synchronize()
 					end
-				end
-
-				iterFunction = function()
-					advancePastPlaceholders()
-
-					-- Load into storedSamples until we find the next sample in
-					-- the sequence or we run out of samples
-					while storedSamples[curSampleIdx] == nil and threads:hasjob() do
-						enqueue()
-						threads:dojob()
-						if threads:haserror() then
-							threads:synchronize()
-						end
-						enqueue()
-
-						storedSamples[sampleOrigIdx] = sample
-
-						advancePastPlaceholders()
-					end
-
 					enqueue()
 
-					local curSample = storedSamples[curSampleIdx]
-					storedSamples[curSampleIdx] = nil
+					storedSamples[sampleOrigIdx] = sample
 
-					curSampleIdx = curSampleIdx + 1
-
-					return curSample
+					advancePastPlaceholders()
 				end
-			else
-				iterFunction = function()
-					while threads:hasjob() do
-						enqueue()
-						threads:dojob()
-						if threads:haserror() then
-							threads:synchronize()
-						end
-						enqueue()
 
-						if table.exact_length(sample) > 0 then
-							return sample
-						end
+				enqueue()
+
+				local curSample = storedSamples[curSampleIdx]
+				storedSamples[curSampleIdx] = nil
+
+				curSampleIdx = curSampleIdx + 1
+
+				return curSample
+			end
+		else
+			iterFunction = function()
+				while threads:hasjob() do
+					enqueue()
+					threads:dojob()
+					if threads:haserror() then
+						threads:synchronize()
+					end
+					enqueue()
+
+					if table.exact_length(sample) > 0 then
+						return sample
 					end
 				end
 			end
-
-			return iterFunction
 		end
+
+		return iterFunction
 	end
+end
 }
