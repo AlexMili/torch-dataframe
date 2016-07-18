e48a79bc2cd445e528b1586ff54ce02b159d9426
Max Gordon <max@gforge.se>
2016-07-18 13:30:18 +0200

    Added loader helper utility that sorts the files in alphabetic order solving dependency between classes Fixed the docs

---
+++ doc/array.md
@@ -0,0 +1,7 @@
+
+## Df_Array
+
+The Df_Array is a class that is used to wrap an array table. An array table
+no key names, it only uses numbers for indexing and each element has to be
+an atomic element, i.e. it may not contain any tables.
+
--- utils.lua
+++ utils.lua
@@ -2,7 +2,7 @@
 
 local argcheck = require "argcheck"
 local doc = require "argcheck.doc"
-
+local paths = require "paths"
 doc[[
 
 ## Utility functions
@@ -298,3 +298,32 @@ if (itorch ~= nil) then
 	print = print_df
 end
 -- END UTILS
+
+paths.get_sorted_files  = argcheck{
+	doc=[[
+<a name="table.get_sorted_lua_files">
+### table.get_sorted_lua_files(@ARGP)
+
+Calls the `paths.files()` with the directory and sorts the files according to
+name.
+
+@ARGT
+
+_Return value_: table with sorted file names
+]],
+	{name="path", type="string",
+	 doc="The directory path"},
+	{name="match_str", type="string", default="[.]lua$",
+	 doc="The file matching string to search for. Defaults to lua file endings."},
+	call=function(path, match_str)
+	local files = {}
+	for f in paths.files(path) do
+	  if (f:match(match_str)) then
+	    files[#files + 1] = f
+	  end
+	end
+
+	table.sort(files)
+
+	return files
+end}
