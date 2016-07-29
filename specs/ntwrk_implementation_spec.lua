64bda47f7e196750eda83a8fbefd5c18c2f32b97
Max Gordon <max@gforge.se>
2016-07-29 10:46:23 +0200

    Fixed network tests so that they pass

--- specs/ntwrk_implementation_spec.lua
+++ specs/ntwrk_implementation_spec.lua
@@ -80,7 +80,7 @@ describe([[
     assert.are.equal(seq_err, total_err)
   end)
 
-  it("Multiple input and multiplpe targets", function()
+  it("Multiple input and #multiple targets", function()
     a = Dataframe("./data/realistic_29_row_data.csv")
     a:create_subsets()
     a:as_categorical("Gender")
@@ -105,7 +105,7 @@ describe([[
     net:add(nn.Linear(10,50))
 
     local prl = nn.ConcatTable()
-    local criterion = nn.ParallelIgnoreCriterion()
+    local criterion = nn.ParallelCriterion()
 
     for i=1,2 do
       subnet = nn.Sequential():
@@ -118,11 +118,23 @@ describe([[
     net:add(prl)
 
     net:forward(data)
-    local total_err = criterion:forward(net.output, label)
+  
+    local _label_ = {}
+    for i=1,label:size(2) do
+      _label_[i] = label:select(2,i):reshape(label:size(1),1)
+    end
+
+
+    local total_err = criterion:forward(net.output, _label_)
+
     local seq_err = {}
     for i=1,#batch do
+      local _row_ = {
+        _label_[1][i],
+        _label_[2][i]
+      }
       net:forward(data[i])
-      seq_err[#seq_err + 1] = criterion:forward(net.output, label[i])
+      seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
     end
     seq_err = torch.Tensor(seq_err):mean()
 
