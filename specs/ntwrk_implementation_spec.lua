a9c3229f6067d65245dcf11bbbeb792aa470cf07
Max Gordon <max@gforge.se>
2016-07-30 07:16:21 +0200

    Added skip lua 51 to network specs Improved the network spec descriptions

--- specs/ntwrk_implementation_spec.lua
+++ specs/ntwrk_implementation_spec.lua
@@ -12,133 +12,143 @@ dofile('init.lua')
 lfs.chdir("specs")
 
 describe([[
-	See if we can get a #network to play nicely with a the Batchframe generated tensors
+	See if we can get #network and other tensor implementations
+	#skip_version_LUA51
 ]], function()
-	it("A single input and a single target", function()
-		a = Dataframe("./data/realistic_29_row_data.csv")
-		a:create_subsets()
-		a:as_categorical("Gender")
+	describe([[
+		Single target with a the Batchframe generated tensors
+	]], function()
+		it("A single input and classifier", function()
+			a = Dataframe("./data/realistic_29_row_data.csv")
+			a:create_subsets()
+			a:as_categorical("Gender")
 
-		local batch = a["/train"]:get_batch(5,
-		Df_Tbl({
-			data = Df_Array("Weight"),
-			label = Df_Array("Gender")
-		}))
+			local batch = a["/train"]:get_batch(5,
+			Df_Tbl({
+				data = Df_Array("Weight"),
+				label = Df_Array("Gender")
+			}))
 
-		local data, label =
+			local data, label =
 			batch:to_tensor()
 
-		require 'nn'
-		local net = nn.Sequential():add(nn.Linear(1,2))
-		local criterion = nn.CrossEntropyCriterion()
+			require 'nn'
+			local net = nn.Sequential():add(nn.Linear(1,2))
+			local criterion = nn.CrossEntropyCriterion()
 
-		net:forward(data)
-		local total_err = criterion:forward(net.output, label)
+			net:forward(data)
+			local total_err = criterion:forward(net.output, label)
 
-		local seq_err = {}
-		for i=1,#batch do
-			net:forward(data[i])
-			seq_err[#seq_err + 1] = criterion:forward(net.output, label[i])
-		end
-		seq_err = torch.Tensor(seq_err):mean()
+			local seq_err = {}
+			for i=1,#batch do
+				net:forward(data[i])
+				seq_err[#seq_err + 1] = criterion:forward(net.output, label[i])
+			end
+			seq_err = torch.Tensor(seq_err):mean()
 
-		assert.is_true(math.abs(seq_err - total_err) < 10^-6)
-	end)
+			assert.is_true(math.abs(seq_err - total_err) < 10^-6)
+		end)
 
-	it("A a tensor input and a #linear_regression", function()
-		a = Dataframe("./data/realistic_29_row_data.csv")
-		a:create_subsets()
-		torch.manualSeed(2313)
+		it("A a tensor input and a #linear_regression", function()
+			a = Dataframe("./data/realistic_29_row_data.csv")
+			a:create_subsets()
+			torch.manualSeed(2313)
 
-		local batch = a["/train"]:get_batch(5,
-		Df_Tbl({
-			data = function(row)
-				return torch.rand(10)
-			end,
-			label = Df_Array("Weight")
-		}))
+			local batch = a["/train"]:get_batch(5,
+			Df_Tbl({
+				data = function(row)
+					return torch.rand(10)
+				end,
+				label = Df_Array("Weight")
+			}))
 
-		local data, label =
+			local data, label =
 			batch:to_tensor()
 
-		require 'nn'
-		local net = nn.Sequential():
+			require 'nn'
+			local net = nn.Sequential():
 			add(nn.Linear(10,1))
-		local criterion = nn.MSECriterion()
+			local criterion = nn.MSECriterion()
 
-		net:forward(data)
-		local total_err = criterion:forward(net.output, label)
+			net:forward(data)
+			local total_err = criterion:forward(net.output, label)
 
-		local seq_err = {}
-		for i=1,#batch do
-			net:forward(data[i])
-			seq_err[#seq_err + 1] = criterion:forward(net.output, label[i])
-		end
-		seq_err = torch.Tensor(seq_err):mean()
+			local seq_err = {}
+			for i=1,#batch do
+				net:forward(data[i])
+				seq_err[#seq_err + 1] = criterion:forward(net.output, label[i])
+			end
+			seq_err = torch.Tensor(seq_err):mean()
 
-		assert.is_true(math.abs(seq_err - total_err) < 10^-6)
+			assert.is_true(math.abs(seq_err - total_err) < 10^-6)
+		end)
 	end)
 
-	it("Multiple input and #multiple regression targets", function()
-		a = Dataframe("./data/realistic_29_row_data.csv")
-		a:create_subsets()
-		torch.manualSeed(9823719)
 
-		local batch = a["/train"]:get_batch(5,
-		Df_Tbl({
-			data = function(row)
-				return torch.rand(10)
-			end,
-			label = function(row)
-				return torch.rand(2)
-			end,
-			label_shape = "NxMx1"
-		}))
+	describe([[
+		Multiple targets with a the Batchframe generated tensors
+		#multiple
+	]], function()
+		it("Regression targets #multreg", function()
+			a = Dataframe("./data/realistic_29_row_data.csv")
+			a:create_subsets()
+			torch.manualSeed(9823719)
 
-		local data, label =
+			local batch = a["/train"]:get_batch(5,
+			Df_Tbl({
+				data = function(row)
+					return torch.rand(10)
+				end,
+				label = function(row)
+					return torch.rand(2)
+				end,
+				label_shape = "NxMx1"
+			}))
+
+			local data, label =
 			batch:to_tensor()
 
-		require 'nn'
-		local net = nn.Sequential()
-		net:add(nn.Linear(10,50))
+			require 'nn'
+			local net = nn.Sequential()
+			net:add(nn.Linear(10,50))
 
-		local prl = nn.ConcatTable()
-		local criterion = nn.ParallelCriterion()
+			local prl = nn.ConcatTable()
+			local criterion = nn.ParallelCriterion()
 
-		for i=1,2 do
-			subnet = nn.Sequential():
-					add(nn.Linear(50,1))
-			criterion:add(nn.MSECriterion())
+			for i=1,2 do
+				subnet = nn.Sequential():
+				add(nn.Linear(50,1))
+				criterion:add(nn.MSECriterion())
 
-			prl:add(subnet)
-		end
+				prl:add(subnet)
+			end
 
-		net:add(prl)
+			net:add(prl)
 
-		net:forward(data)
+			net:forward(data)
 
-		local total_err = criterion:forward(net.output, label)
+			local total_err = criterion:forward(net.output, label)
 
-		local seq_err = {}
-		for i=1,#batch do
-			local _row_ = {
-				label[1][i],
-				label[2][i]
-			}
-			net:forward(data[i])
-			seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
-		end
-		seq_err = torch.Tensor(seq_err):mean()
+			local seq_err = {}
+			for i=1,#batch do
+				local _row_ = {
+					label[1][i],
+					label[2][i]
+				}
+				net:forward(data[i])
+				seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
+			end
+			seq_err = torch.Tensor(seq_err):mean()
 
-		assert.is_true(math.abs(seq_err - total_err) < 10^-6)
-	end)
+			assert.is_true(math.abs(seq_err - total_err) < 10^-6)
+		end)
 
-	it("Multiple input and #multiple classification targets #mclss", function()
-		a = Dataframe("./data/realistic_29_row_data.csv")
-		a:create_subsets()
-		a:as_categorical("Gender")
-		a:add_column("Overweight")
-		a:update(
+		it("Classification targets #multclss", function()
+			a = Dataframe("./data/realistic_29_row_data.csv")
+			a:create_subsets()
+			a:as_categorical("Gender")
+			a:add_column("Overweight")
+			a:update(
 			function(row) return true end,
 			function(row)
 				if (row["Weight"] >= 25) then
@@ -149,54 +159,54 @@ describe([[
 
 				return row
 			end)
-		torch.manualSeed(823609)
+			torch.manualSeed(823609)
 
-		local batch = a["/train"]:get_batch(5,
-		Df_Tbl({
-			data = function(row)
-				return torch.rand(10)
-			end,
-			label = Df_Array("Gender", "Overweight"),
-			label_shape = "NxM"
-		}))
+			local batch = a["/train"]:get_batch(5,
+			Df_Tbl({
+				data = function(row)
+					return torch.rand(10)
+				end,
+				label = Df_Array("Gender", "Overweight"),
+				label_shape = "NxM"
+			}))
 
-		local data, label =
+			local data, label =
 			batch:to_tensor()
 
-		require 'nn'
-		local net = nn.Sequential()
-		net:add(nn.Linear(10,50))
+			require 'nn'
+			local net = nn.Sequential()
+			net:add(nn.Linear(10,50))
 
-		local prl = nn.ConcatTable()
-		local criterion = nn.ParallelCriterion()
+			local prl = nn.ConcatTable()
+			local criterion = nn.ParallelCriterion()
 
-		for i=1,2 do
-			subnet = nn.Sequential():
-					add(nn.Linear(50,2)):
-					add(nn.LogSoftMax())
-			criterion:add(nn.ClassNLLCriterion())
+			for i=1,2 do
+				subnet = nn.Sequential():
+				add(nn.Linear(50,2)):
+				add(nn.LogSoftMax())
+				criterion:add(nn.ClassNLLCriterion())
 
-			prl:add(subnet)
-		end
+				prl:add(subnet)
+			end
 
-		net:add(prl)
+			net:add(prl)
 
-		net:forward(data)
+			net:forward(data)
 
-		local total_err = criterion:forward(net.output, label)
+			local total_err = criterion:forward(net.output, label)
 
-		local seq_err = {}
-		for i=1,#batch do
-			local _row_ = {
-				label[1][i],
-				label[2][i]
-			}
-			net:forward(data[i])
-			seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
-		end
-		seq_err = torch.Tensor(seq_err):mean()
+			local seq_err = {}
+			for i=1,#batch do
+				local _row_ = {
+					label[1][i],
+					label[2][i]
+				}
+				net:forward(data[i])
+				seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
+			end
+			seq_err = torch.Tensor(seq_err):mean()
 
-		assert.is_true(math.abs(seq_err - total_err) < 10^-6)
+			assert.is_true(math.abs(seq_err - total_err) < 10^-6)
+		end)
 	end)
-
 end)
