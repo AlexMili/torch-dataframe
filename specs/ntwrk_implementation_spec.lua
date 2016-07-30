f5fe3678406b98c967d2b5a38daa6707c2d03fa3
Max Gordon <max@gforge.se>
2016-07-30 11:38:05 +0200

    Added pure iterator example Fixed indentation

--- specs/ntwrk_implementation_spec.lua
+++ specs/ntwrk_implementation_spec.lua
@@ -209,143 +209,281 @@ describe([[
 		end)
 	end)
 
-
-		describe([[
-			Multiple targets with a the parallel_iterator for generating tensors
-			#parallel_iterator
-		]], function()
-			it("Regression targets #multreg", function()
-				a = Dataframe("./data/realistic_29_row_data.csv")
-				a:create_subsets{
-					class_args = Df_Tbl{
-						batch_args = Df_Tbl{
-							label_shape = "NxMx1"
-						}
+	describe([[
+		Multiple targets with a the parallel_iterator for generating tensors
+		#parallel_iterator
+	]], function()
+		it("Regression targets #multreg", function()
+			a = Dataframe("./data/realistic_29_row_data.csv")
+			a:create_subsets{
+				class_args = Df_Tbl{
+					batch_args = Df_Tbl{
+						label_shape = "NxMx1"
 					}
 				}
-				torch.manualSeed(9823719)
+			}
+			torch.manualSeed(9823719)
 
-				local iterator = a["/train"]:
-					set_data_retriever(function(row)
-						return torch.rand(10)
-					end):
-					set_label_retriever(function(row)
-						return torch.rand(2)
-					end):
-					get_parallel_iterator{
-						batch_size = 5,
-						nthread = 2}
+			local iterator = a["/train"]:
+				set_data_retriever(function(row)
+					return torch.rand(10)
+				end):
+				set_label_retriever(function(row)
+					return torch.rand(2)
+				end):
+				get_parallel_iterator{
+					batch_size = 5,
+					nthread = 2}
 
-				local sample = iterator()()
-				local data = sample.input
-				local label = sample.target
+			local sample = iterator()()
+			local data = sample.input
+			local label = sample.target
 
-				require 'nn'
-				local net = nn.Sequential()
-				net:add(nn.Linear(10,50))
+			require 'nn'
+			local net = nn.Sequential()
+			net:add(nn.Linear(10,50))
 
-				local prl = nn.ConcatTable()
-				local criterion = nn.ParallelCriterion()
+			local prl = nn.ConcatTable()
+			local criterion = nn.ParallelCriterion()
 
-				for i=1,2 do
-					subnet = nn.Sequential():
-					add(nn.Linear(50,1))
-					criterion:add(nn.MSECriterion())
+			for i=1,2 do
+				subnet = nn.Sequential():
+				add(nn.Linear(50,1))
+				criterion:add(nn.MSECriterion())
 
-					prl:add(subnet)
-				end
+				prl:add(subnet)
+			end
 
-				net:add(prl)
+			net:add(prl)
 
-				net:forward(data)
+			net:forward(data)
 
-				local total_err = criterion:forward(net.output, label)
+			local total_err = criterion:forward(net.output, label)
 
-				local seq_err = {}
-				for i=1,data:size(1) do
-					local _row_ = {
-						label[1][i],
-						label[2][i]
-					}
-					net:forward(data[i])
-					seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
-				end
-				seq_err = torch.Tensor(seq_err):mean()
-
-				assert.near(seq_err, total_err, 10^-6, "Errors are not identical when running alone or in batch")
-			end)
-
-			it("Classification targets #multclss", function()
-				a = Dataframe("./data/realistic_29_row_data.csv")
-				a:create_subsets{
-					class_args = Df_Tbl{
-						batch_args = Df_Tbl{
-							label_shape = "NxM"
-						}
-					}
+			local seq_err = {}
+			for i=1,data:size(1) do
+				local _row_ = {
+					label[1][i],
+					label[2][i]
 				}
-				a:as_categorical("Gender")
-				a:add_column("Overweight")
-				a:update(
-				function(row) return true end,
-				function(row)
-					if (row["Weight"] >= 25) then
-						row["Overweight"] = 1
-					else
-						row["Overweight"] = 0
-					end
+				net:forward(data[i])
+				seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
+			end
+			seq_err = torch.Tensor(seq_err):mean()
 
-					return row
-				end)
-				torch.manualSeed(823609)
-
-				local iterator = a["/train"]:
-					set_data_retriever(function(row)
-						return torch.rand(10)
-					end):
-					set_label_retriever(Df_Array("Gender", "Overweight")):
-					get_parallel_iterator{
-						batch_size = 5,
-						nthread = 2}
-
-				local sample = iterator()()
-				local data = sample.input
-				local label = sample.target
-
-				require 'nn'
-				local net = nn.Sequential()
-				net:add(nn.Linear(10,50))
-
-				local prl = nn.ConcatTable()
-				local criterion = nn.ParallelCriterion()
-
-				for i=1,2 do
-					subnet = nn.Sequential():
-					add(nn.Linear(50,2)):
-					add(nn.LogSoftMax())
-					criterion:add(nn.ClassNLLCriterion())
-
-					prl:add(subnet)
-				end
-
-				net:add(prl)
-
-				net:forward(data)
-
-				local total_err = criterion:forward(net.output, label)
-
-				local seq_err = {}
-				for i=1,data:size(1) do
-					local _row_ = {
-						label[1][i],
-						label[2][i]
-					}
-					net:forward(data[i])
-					seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
-				end
-				seq_err = torch.Tensor(seq_err):mean()
-
-				assert.near(seq_err, total_err, 10^-6, "Errors are not identical when running alone or in batch")
-			end)
+			assert.near(seq_err, total_err, 10^-6, "Errors are not identical when running alone or in batch")
 		end)
+
+		it("Classification targets #multclss", function()
+			a = Dataframe("./data/realistic_29_row_data.csv")
+			a:create_subsets{
+				class_args = Df_Tbl{
+					batch_args = Df_Tbl{
+						label_shape = "NxM"
+					}
+				}
+			}
+			a:as_categorical("Gender")
+			a:add_column("Overweight")
+			a:update(
+			function(row) return true end,
+			function(row)
+				if (row["Weight"] >= 25) then
+					row["Overweight"] = 1
+				else
+					row["Overweight"] = 0
+				end
+
+				return row
+			end)
+			torch.manualSeed(823609)
+
+			local iterator = a["/train"]:
+				set_data_retriever(function(row)
+					return torch.rand(10)
+				end):
+				set_label_retriever(Df_Array("Gender", "Overweight")):
+				get_parallel_iterator{
+					batch_size = 5,
+					nthread = 2}
+
+			local sample = iterator()()
+			local data = sample.input
+			local label = sample.target
+
+			require 'nn'
+			local net = nn.Sequential()
+			net:add(nn.Linear(10,50))
+
+			local prl = nn.ConcatTable()
+			local criterion = nn.ParallelCriterion()
+
+			for i=1,2 do
+				subnet = nn.Sequential():
+				add(nn.Linear(50,2)):
+				add(nn.LogSoftMax())
+				criterion:add(nn.ClassNLLCriterion())
+
+				prl:add(subnet)
+			end
+
+			net:add(prl)
+
+			net:forward(data)
+
+			local total_err = criterion:forward(net.output, label)
+
+			local seq_err = {}
+			for i=1,data:size(1) do
+				local _row_ = {
+					label[1][i],
+					label[2][i]
+				}
+				net:forward(data[i])
+				seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
+			end
+			seq_err = torch.Tensor(seq_err):mean()
+
+			assert.near(seq_err, total_err, 10^-6, "Errors are not identical when running alone or in batch")
+		end)
+	end)
+
+	describe([[
+		Multiple targets with a the iterator for generating tensors
+		#iterator
+	]], function()
+		it("Regression targets #multreg", function()
+			a = Dataframe("./data/realistic_29_row_data.csv")
+			a:create_subsets{
+				class_args = Df_Tbl{
+					batch_args = Df_Tbl{
+						label_shape = "NxMx1"
+					}
+				}
+			}
+			torch.manualSeed(9823719)
+
+			local iterator = a["/train"]:
+				set_data_retriever(function(row)
+					return torch.rand(10)
+				end):
+				set_label_retriever(function(row)
+					return torch.rand(2)
+				end):
+				get_iterator{
+					batch_size = 5
+				}
+
+			local sample = iterator()()
+			local data = sample.input
+			local label = sample.target
+
+			require 'nn'
+			local net = nn.Sequential()
+			net:add(nn.Linear(10,50))
+
+			local prl = nn.ConcatTable()
+			local criterion = nn.ParallelCriterion()
+
+			for i=1,2 do
+				subnet = nn.Sequential():
+				add(nn.Linear(50,1))
+				criterion:add(nn.MSECriterion())
+
+				prl:add(subnet)
+			end
+
+			net:add(prl)
+
+			net:forward(data)
+
+			local total_err = criterion:forward(net.output, label)
+
+			local seq_err = {}
+			for i=1,data:size(1) do
+				local _row_ = {
+					label[1][i],
+					label[2][i]
+				}
+				net:forward(data[i])
+				seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
+			end
+			seq_err = torch.Tensor(seq_err):mean()
+
+			assert.near(seq_err, total_err, 10^-6, "Errors are not identical when running alone or in batch")
+		end)
+
+		it("Classification targets #multclss", function()
+			a = Dataframe("./data/realistic_29_row_data.csv")
+			a:create_subsets{
+				class_args = Df_Tbl{
+					batch_args = Df_Tbl{
+						label_shape = "NxM"
+					}
+				}
+			}
+			a:as_categorical("Gender")
+			a:add_column("Overweight")
+			a:update(
+			function(row) return true end,
+			function(row)
+				if (row["Weight"] >= 25) then
+					row["Overweight"] = 1
+				else
+					row["Overweight"] = 0
+				end
+
+				return row
+			end)
+			torch.manualSeed(823609)
+
+			local iterator = a["/train"]:
+				set_data_retriever(function(row)
+					return torch.rand(10)
+				end):
+				set_label_retriever(Df_Array("Gender", "Overweight")):
+				get_iterator{
+					batch_size = 5
+				}
+
+			local sample = iterator()()
+			local data = sample.input
+			local label = sample.target
+
+			require 'nn'
+			local net = nn.Sequential()
+			net:add(nn.Linear(10,50))
+
+			local prl = nn.ConcatTable()
+			local criterion = nn.ParallelCriterion()
+
+			for i=1,2 do
+				subnet = nn.Sequential():
+				add(nn.Linear(50,2)):
+				add(nn.LogSoftMax())
+				criterion:add(nn.ClassNLLCriterion())
+
+				prl:add(subnet)
+			end
+
+			net:add(prl)
+
+			net:forward(data)
+
+			local total_err = criterion:forward(net.output, label)
+
+			local seq_err = {}
+			for i=1,data:size(1) do
+				local _row_ = {
+					label[1][i],
+					label[2][i]
+				}
+				net:forward(data[i])
+				seq_err[#seq_err + 1] = criterion:forward(net.output, _row_)
+			end
+			seq_err = torch.Tensor(seq_err):mean()
+
+			assert.near(seq_err, total_err, 10^-6, "Errors are not identical when running alone or in batch")
+		end)
+	end)
 end)
